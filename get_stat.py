870df943b2a57712f44e6ce6239bab4902fb2330
EgorEvgenievich <judoegor14@gmail.com>
2019-12-22 23:23:38 +0300

    Add right output

--- get_stat.py
+++ get_stat.py
@@ -9,7 +9,7 @@ def arg_parser():
 	parser.add_argument("-f","--fullnames",action='append',help="display listener with name <fulname> marks")
 	parser.add_argument("-s","--subjects",action='append',help="display journal for <subject>")
 	parser.add_argument("-p",action='append',help="display pz for subject")
-	parser.add_argument("-j","--json",help="display in JSON format")
+	parser.add_argument("-j","--json",action="store_true",help="display in JSON format")
 	return parser.parse_args()
 
 class Storage():
@@ -19,23 +19,68 @@ class Storage():
         db = self._client["listners"]
         return db["listners"]
 
-    def get_stat(self,fullnames=None,subjects=None,p=None):
+    def get_avg_mark(self, fullnames=None, subjects=None, json=False):
+        client = Connect().get_connection()
+        db = client["listners"]
+        col = db["listners"]
+        if fullnames == None:
+            fullnames = col.distinct("fullname")
+        if subjects == None:
+            subjects = col.distinct("subjects.subject")
+        for fullname in fullnames:
+
+            for subject in subjects:
+                pipeline = [{"$match": {"fullname": fullname}}, {"$unwind": "$subjects"},
+                            {"$match": {"subjects.subject": subject}},
+                            {"$project": {"_id": 0, "fullname": 1, "subject": "$subjects.subject",
+                                          "mark": "$subjects.mark"
+                                , "maxmark": "$subjects.maxmark", "count": {"$add": [1]}}},
+                            {"$group": {"_id": {"fullname": "$fullname", "subject": "$subject"},
+                                        "average mark": {"$avg": "$mark"}}},
+
+                           ]
+                if json:
+                    for x in col.aggregate(pipeline):
+                        pprint(x)
+                else:
+                    for x in col.aggregate(pipeline):
+                        print("Listner: {fullname}\tSubject: {subject}\tAverage Mark: {mark}".format(
+                            fullname=x["_id"]["fullname"],subject=x["_id"]["subject"],mark=x["average mark"]))
+
+    def get_stat(self,fullnames=None,subjects=None,p=None,json=False):
         col=self.get_col()
         if fullnames ==None:
             fullnames=col.distinct("fullname")
         if fullnames and not subjects and not p:
             for fullname in fullnames:
-                pipeline = [{"$match": {"fullname": fullname}}]
-                for x in col.aggregate(pipeline):
-                    pprint(x)
+                pipeline = [{"$match": {"fullname": fullname}},{"$project":{"_id":0}}]
+                if json:
+                    for x in col.aggregate(pipeline):
+                        pprint(x)
+                else:
+                    pipeline=[{"$match": {"fullname": fullname}},{"$project":{"_id":0}},{"$unwind":"$subjects"}]
+                    for x in col.aggregate(pipeline):
+                        print("{0} {1} {2} {3} {4} {5} {6} {7} {8}".format(
+                            x["fullname"],x["subjects"]["subject"],x["subjects"]["pz"],x["subjects"]["date"],
+                            x["subjects"]["time"], x["subjects"]["mark"],x["subjects"]["maxmark"],
+                            x["subjects"]["exec_time"],x["subjects"]["ip_addr" ]))
+
             exit(0)
         if subjects and not p:
             for fullname in fullnames:
                 for subject in subjects:
                     pipeline = [{"$match": {"fullname":fullname}},{"$unwind":"$subjects"},
                                 {"$match":{"subjects.subject":subject}}]
-                    for x in col.aggregate(pipeline):
-                        pprint(x)
+                    if json:
+                        for x in col.aggregate(pipeline):
+                            pprint(x)
+                    else:
+                        for x in col.aggregate(pipeline):
+                            print("{0} {1} {2} {3} {4} {5} {6} {7} {8}".format(
+                                x["fullname"], x["subjects"]["subject"], x["subjects"]["pz"], x["subjects"]["date"],
+                                x["subjects"]["time"], x["subjects"]["mark"], x["subjects"]["maxmark"],
+                                x["subjects"]["exec_time"], x["subjects"]["ip_addr"]))
+
             exit(0)
         if subjects and p:
             for fullname in fullnames:
@@ -43,64 +88,26 @@ class Storage():
                     for pz in p:
                         pipeline = [{"$match": {"fullname":fullname}},{"$unwind":"$subjects"},
                                 {"$match":{"subjects.subject":subject,"subjects.pz":pz}}]
-                        for x in col.aggregate(pipeline):
-                            pprint(x)
+                        if json:
+                            for x in col.aggregate(pipeline):
+                                pprint(x)
+                        else:
+                            for x in col.aggregate(pipeline):
+                                print("{0} {1} {2} {3} {4} {5} {6} {7} {8}".format(
+                                    x["fullname"], x["subjects"]["subject"], x["subjects"]["pz"], x["subjects"]["date"],
+                                    x["subjects"]["time"], x["subjects"]["mark"], x["subjects"]["maxmark"],
+                                    x["subjects"]["exec_time"], x["subjects"]["ip_addr"]))
             exit(0)
 
-    def get_avg_mark(self, fullnames=None, subjects=None ):
-        client=Connect().get_connection()
-        db=client["listners"]
-        col=db["listners"]
-        if fullnames==None:
-            fullnames=col.distinct("fullname")
-        if subjects==None:
-            subjects=col.distinct("subjects.subject")
-        for fullname in fullnames:
-            for subject in subjects:
-                pipeline=[{"$match":{"fullname":fullname}},{"$unwind":"$subjects"},
-                          {"$match":{"subjects.subject":subject}},
-                          {"$project":{"_id":0,"fullname":1,"subject":"$subjects.subject","mark":"$subjects.mark"
-                                        ,"maxmark":"$subjects.maxmark","count":{"$add":[1]}}},
-                         {"$group": {"_id":{"fullname":"$fullname","subject":"$subject"},
-                          "average mark": {"$avg":"$mark"}}}
-                ]
-                for x in col. aggregate(pipeline):
-                    pprint(x)
-                # pprint(col.aggregate(pipeline))
+
 
 def main():
     args= arg_parser()
     storage=Storage()
     args=arg_parser()
     if args.average:
-        storage.get_avg_mark(args.fullnames,args.subjects)
+        storage.get_avg_mark(args.fullnames,args.subjects,args.json)
         exit(0)
-    pprint(args)
-    storage.get_stat(fullnames=args.fullnames,subjects=args.subjects,p=args.p)
+    storage.get_stat(fullnames=args.fullnames,subjects=args.subjects,p=args.p,json=args.json)
 main()
-# dict(args).
-# if len(args.fullname) and not len(args.p) and not len(args.p) :
-#     for fullname in args.fullname:
-#         pipeline=[{"$match":{"fullname":fullname}}]
-#         print(pipeline)
-#         for x in col.aggregate(pipeline):
-#             pprint(x)
-#     exit(0)
-# if args.subject:
-#     for subject in args.subject:
-#         for pz in args.p:
-#             pipeline=[{"$unwind":"$subjects"},{"$unwind":"$subjects."+subject}]
-#         for x in col.aggregate(pipeline):
-#             pprint(x)
-#     exit(0)
-# if args.p :
-#     for subject in args.subject:
-#         for pz in args.p:
-#             pipeline=[{"$unwind":"$subjects"},{"$unwind":"$subjects."+subject},
-#                 {"$match":{"subjects."+subject+".pz":pz}}]
-#         for x in col.aggregate(pipeline):
-#             pprint(x)
-#     exit(0)
-# for x in col.find():
-#     pprint(x)
-# print(type(args))
+
